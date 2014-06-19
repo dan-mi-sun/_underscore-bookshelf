f7f8d39afe920b73b19bf43c45f636175c726516
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-19 13:15:12 +0100

    0. Use _.each to iterate over each book and display it in the bookshelf.

--- javascripts/books.js
+++ javascripts/books.js
@@ -1,3 +1,23 @@
+ var books =[]
+
 function handleResponse(response) {
-  console.log(response.items);
+  _(response.items).each(function (book) {
+    var bookObj = new Book(book);
+    books.push(bookObj);
+    bookObj.add_to_bookshelf();
+  });
+}
+
+function Book(book_info) {
+  this.image_url = book_info.volumeInfo.imageLinks.smallThumbnail;
+  this.title = book_info.volumeInfo.title
+  this.rating = book_info.volumeInfo.averageRating;
+}
+
+Book.prototype.add_to_bookshelf = function() {
+  var bookshelf = $(document.getElementById("bookshelf"));
+  var img = $(document.createElement("img"));
+  img.attr("src", this.image_url);
+  img.data("title", this.title);
+  bookshelf.append(img);
 }
